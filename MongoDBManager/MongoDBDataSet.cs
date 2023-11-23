431debb46f38f3979f48ab0d1fc3e203c30d5962
Tom Vande Wiele <tvdewiel@hotmail.com>
2023-11-23 11:03:43 +0100

    Add project files.

---
+++ MongoDBManager/MongoDBDataSet.cs
@@ -0,0 +1,31 @@
+﻿using DataSetManager;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoDBManager
+{
+    public class MongoDBDataSet
+    {
+        public MongoDBDataSet(DataSetMetaInfo metaInfo, DataSet dataSet)
+        {
+            this.metaInfo = metaInfo;
+            this.dataSet = dataSet;
+        }
+
+        public MongoDBDataSet(ObjectId id, DataSetMetaInfo metaInfo, DataSet dataSet)
+        {
+            this.id = id;
+            this.metaInfo = metaInfo;
+            this.dataSet = dataSet;
+        }
+
+        public ObjectId id {  get; set; }
+        public DataSetMetaInfo metaInfo { get; set; }
+        public DataSet dataSet { get; set; }
+    }
+}
---
+++ MongoDBManagerTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using DataSetManager;
+using MongoDBManager;
+
+namespace MongoDBManagerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string source=@"c:\data\belgica\belgica.txt";
+            string campaignCode = "Juli 2022";
+            string dataSeries = "ModelTest ID A";
+
+            string connectionString = "mongodb://localhost:27017";
+            MongoDBDataSetRepository repository = new MongoDBDataSetRepository(connectionString);
+
+            var data = FileDataSetManager.ReadData(@"C:\data\belgica\belgica.txt");
+            var sets = FileDataSetManager.MakeDataSetsWithTestSet(data.data, new List<int> { 5000, 5000, 2500, 15000, 1000 });
+
+            DataSetMetaInfo metaInfo = new DataSetMetaInfo(sets[0].data.Count,source,campaignCode,dataSeries,DataSetType.TestSet);
+
+            List<MongoDBDataSet> mongoDBDataSets = new List<MongoDBDataSet>();
+            MongoDBDataSet mdsSet = new MongoDBDataSet(metaInfo,sets[0]);
+            mongoDBDataSets.Add(mdsSet);
+
+            for(int i=1;i<sets.Count;i++)
+            {
+                metaInfo = new DataSetMetaInfo(sets[i].data.Count, source, campaignCode, dataSeries, DataSetType.DataSet);
+                mdsSet=new MongoDBDataSet(metaInfo, sets[i]);
+                mongoDBDataSets.Add(mdsSet);
+            }
+
+            repository.WriteDataSets(mongoDBDataSets);
+            Console.WriteLine("end");
+        }
+    }
+}
